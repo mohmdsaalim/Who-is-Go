7a84c7eb32d4d69d15a0dcba0d31632d51c45f09
mohmdsaalim <muhammedsaalim005@gmail.com>
2026-02-02 14:21:07 +0530

    interface-revised

---
+++ master-IN-interfaces/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface{
+	Area() float64
+}
+
+type Rectangle struct {
+	width, height float64
+}
+type Circle struct {
+	radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func calculateArea(r Shape) float64 {
+	return r.Area()
+}
+func main() {
+	rect := Rectangle{width: 10, height: 20}
+	cir := Circle{radius: 10}
+	// a := calculateArea(rect)
+	// b := calculateArea(cir)
+
+	fmt.Println(calculateArea(rect))
+    fmt.Println(calculateArea(cir))
+
+}
